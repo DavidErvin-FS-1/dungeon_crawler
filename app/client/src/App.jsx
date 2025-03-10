851d0abf54e79ebdc833e1de9f734d6044cb4461
David Ervin <dnervin@student.fullsail.edu>
2025-03-10 19:09:23 -0400

    Add Tailwind CSS integration and configure Vite server settings

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <h1>React App</h1>
-    </>
+    <main className='container mx-auto p-4 flex flex-col items-center justify-center h-screen'>
+      <h1 class='text-3xl font-bold underline'>React App</h1>
+    </main>
   )
 }
 
