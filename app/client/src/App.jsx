d70f6217d3fd42cfa9514784e3c6128c7c123622
David Ervin <dnervin@student.fullsail.edu>
2025-03-10 19:14:01 -0400

    Implement routing with react-router in App component and wrap application in BrowserRouter

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from 'react-router'
+
 function App() {
   return (
-    <main className='container mx-auto p-4 flex flex-col items-center justify-center h-screen'>
-      <h1 class='text-3xl font-bold underline'>React App</h1>
-    </main>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <div>
+            <main className='container mx-auto p-4 flex flex-col items-center justify-center h-screen'>
+              <h1 class='text-3xl font-bold underline'>React App</h1>
+            </main>
+          </div>
+        }
+      />
+    </Routes>
   )
 }
 
