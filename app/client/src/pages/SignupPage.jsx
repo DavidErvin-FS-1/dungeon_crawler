f69f9f340b0962be456583834c8a3cc470d8cd84
David Ervin <dnervin@student.fullsail.edu>
2025-03-18 18:27:05 -0400

    Add SignupPage component with form for user registration

---
+++ app/client/src/pages/SignupPage.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { Link } from 'react-router'
+import Button from '../components/Button'
+import Input from '../components/Input'
+
+function SignupPage() {
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  return (
+    <main className='container flex flex-col items-center justify-center gap-4 p-4 mx-auto h-dvh'>
+      <h1 className='text-4xl font-bold'>Login</h1>
+
+      <form onSubmit={handleSubmit} className='form min-w-md'>
+        <div className='form-content'>
+          <label htmlFor='email' className='text-dg-black'>
+            Email
+          </label>
+          <Input
+            type={'text'}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Enter your email'
+          />
+          <label htmlFor='username' className='text-dg-black'>
+            Username
+          </label>
+          <Input
+            type={'text'}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder='Enter your username'
+          />
+          <label htmlFor='password' className='text-dg-black'>
+            Password
+          </label>
+          <Input
+            type={'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Enter your password'
+          />
+
+          <div className='flex items-center justify-between mt-2'>
+            <div className='flex items-center gap-2'>
+              <label htmlFor='remember' className='text-sm text-dg-black'>
+                Remember me
+              </label>
+              <input type='checkbox' id='remember' />
+            </div>
+            <Link to='#' className='text-sm hover:underline text-dg-black'>
+              Forgot password?
+            </Link>
+          </div>
+
+          <div className='flex flex-col justify-center mt-2 gap-2'>
+            <Button
+              label={'Sign Up'}
+              className='btn success text-xl'
+              onClick={handleSubmit}
+            />
+            <Link to='/login' className='btn default w-full text-xl'>
+              <div className='inner-btn text-center'>Log In</div>
+            </Link>
+          </div>
+        </div>
+      </form>
+    </main>
+  )
+}
+
+export default SignupPage
