760bd132be68b160588c47ad7f34f0da2aaf0c46
David Ervin <dnervin@student.fullsail.edu>
2025-03-18 18:27:15 -0400

    Add routing for LoginPage, SignupPage, and AllocateStatsPage in App component

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from 'react-router'
+import AllocateStatsPage from './pages/AllocateStatsPage'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/signup' element={<SignupPage />} />
+      <Route path='/allocate-stats' element={<AllocateStatsPage />} />
     </Routes>
   )
 }
