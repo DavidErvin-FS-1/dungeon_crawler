06be8cfe091b09c034f5735e57abc2ebdfacaf62
David Ervin <dnervin@student.fullsail.edu>
2025-03-18 17:34:30 -0400

    Replace main route with HomePage component in App.jsx

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,45 +1,10 @@
-import { FaCheck, FaXmark } from 'react-icons/fa6'
 import { Route, Routes } from 'react-router'
-import Button from './components/Button'
-import Menu from './components/Menu.jsx'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <Routes>
-      <Route
-        path='/'
-        element={
-          <main className='container flex flex-col items-center justify-center gap-4 p-4 mx-auto h-dvh'>
-            <div className='flex items-center justify-center'>
-              <Menu />
-            </div>
-            <div className='flex items-center justify-center gap-4'>
-              <Button label='Default' className='btn default' />
-              <Button label='Success' className='btn success' />
-              <Button label='Error' className='btn error' />
-              <Button label='Disabled' className='btn disabled' />
-            </div>
-            <div className='flex items-center justify-center gap-4'>
-              <Button
-                label={<FaCheck className='w-4 h-4' />}
-                className='btn icon'
-              />
-              <Button
-                label={<FaCheck className='w-4 h-4' />}
-                className='btn icon success'
-              />
-              <Button
-                label={<FaXmark className='w-4 h-4' />}
-                className='btn icon error'
-              />
-              <Button
-                label={<FaXmark className='w-4 h-4' />}
-                className='btn icon disabled'
-              />
-            </div>
-          </main>
-        }
-      />
+      <Route path='/' element={<HomePage />} />
     </Routes>
   )
 }
