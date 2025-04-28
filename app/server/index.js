89e40378e9b36cf2d972e907d818b78a920b5ad7
David Ervin <dnervin@student.fullsail.edu>
2025-04-27 21:25:42 -0400

    Remove commented-out MongoDB connection code and update mongodb dependency to version 6.16.0

--- app/server/index.js
+++ app/server/index.js
@@ -8,17 +8,6 @@ const connectDB = require("./config/db");
 dotenv.config();
 
 // Connect to MongoDB
-// mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log('Connected to MongoDB')
-//   })
-//   .catch((err) => {
-//     console.error('MongoDB connection error:', err)
-//   })
 connectDB().catch(console.dir);
 
 // Create an Express application
